6e189875cec440f2b70d060b540d7012d22eed1e
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-05-24 21:30:06 -0300

    terminei o requisito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -11,6 +11,7 @@ class Game extends Component {
     this.state = {
       arrayQuest: [],
       index: 0,
+      loading: true,
     };
   }
 
@@ -22,7 +23,7 @@ class Game extends Component {
     const token = localStorage.getItem('token');
     const results = await fetchQuest(token);
     if (results.length) {
-      this.setState({ arrayQuest: results });
+      this.setState({ arrayQuest: results, loading: false });
     } else {
       const { history } = this.props;
       localStorage.setItem('token', '');
@@ -30,25 +31,51 @@ class Game extends Component {
     }
   }
 
-  render() {
+  answers = () => {
     const { arrayQuest, index } = this.state;
+
+    const correctAnswer = (
+      <button
+        data-testid="correct-answer"
+        type="button"
+      >
+        {arrayQuest[index].correct_answer}
+      </button>
+    );
+    const incorrectAnswers = arrayQuest[index].incorrect_answers.map((answer, id) => (
+      <button
+        key={ answer }
+        type="button"
+        data-testid={ `wrong-answer-${id}` }
+      >
+        {answer}
+      </button>
+    ));
+
+    const AnswersArr = [...incorrectAnswers, correctAnswer];
+    const SHUFFLE = 0.5;
+    const sortedAnswers = AnswersArr.sort(() => SHUFFLE - Math.random());
+
+    return sortedAnswers;
+  }
+
+  render() {
+    const { arrayQuest, index, loading } = this.state;
 
     return (
       <div>
         <Header />
-        <div>
-          {arrayQuest.map((quest, id) => (
-            <div key="id">
-              <p key={ id } data-testid="question-category">
-                {arrayQuest[index].category}
-
-              </p>
-              <p key={ id } data-testid="question-text">
-                {arrayQuest[index].question}
-              </p>
+        {loading
+          ? <p>carregando</p>
+          : (
+            <div>
+              <h2 data-testid="question-category">{arrayQuest[index].category}</h2>
+              <h3 data-testid="question-text">{arrayQuest[index].question}</h3>
+              <div data-testid="answer-options">
+                {this.answers().map((answer) => (answer))}
+              </div>
             </div>
-          )).slice(4)}
-        </div>
+          )}
       </div>
     );
   }
