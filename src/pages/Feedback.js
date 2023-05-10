c60254db577144eceec031d77fe362b72e1b53a5
Jonathan Santos <jonathan01347@gmail.com>
2023-05-09 21:39:37 -0300

    refactor(Feedback)Adding materialui resource

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,58 +1,108 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Header from '../componets/Header';
 import { resetState } from '../redux/actions';
+import Footer from '../componets/Footer';
 
 class Feedback extends Component {
   render() {
     const NUMBER_THREE = 3;
     const { rightAnswer, score, assertions, history, reset } = this.props;
     return (
-      <main>
-        <header>
-          <Header />
-        </header>
-        <section>
-          {
-            rightAnswer < NUMBER_THREE ? (
-              <h1 data-testid="feedback-text">Could be better...</h1>
-            ) : (
-              <h1 data-testid="feedback-text">Well Done!</h1>
-            )
-          }
-          <div>
-            <h2>
-              Seu Placar final foi:
-              <span data-testid="feedback-total-score">{score}</span>
-            </h2>
-            <h2>
-              Você acertou :
-              <span data-testid="feedback-total-question">{assertions}</span>
-              perguntas
-            </h2>
-          </div>
-          <button
-            type="button"
-            data-testid="btn-play-again"
-            onClick={ () => {
-              reset();
-              history.push('/');
+      <Box>
+        <Header />
+        <Box
+          sx={ {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100vw',
+            height: '80vh',
+          } }
+        >
+          <Box
+            sx={ {
+              marginBottom: '40px',
             } }
           >
-            Play Again
-          </button>
-          <button
-            type="button"
-            data-testid="btn-ranking"
-            onClick={ () => {
-              history.push('/ranking');
+            {
+              rightAnswer < NUMBER_THREE ? (
+                <Typography
+                  variant="h2"
+                  data-testid="feedback-text"
+                >
+                  Could be better...
+                </Typography>
+              ) : (
+                <Typography
+                  variant="h3"
+                  data-testid="feedback-text"
+                >
+                  Well Done!
+                </Typography>
+              )
+            }
+          </Box>
+          <Box
+            sx={ {
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
             } }
           >
-            Ranking
-          </button>
-        </section>
-      </main>
+            <Typography
+              variant="h4"
+              data-testid="feedback-total-score"
+            >
+              {`Placar final: ${score}`}
+            </Typography>
+            <Typography
+              variant="h6"
+              data-testid="feedback-total-question"
+            >
+              {`Você acertou: ${assertions}`}
+            </Typography>
+          </Box>
+          <Box
+            sx={ {
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: '40px',
+              width: '10vw',
+            } }
+          >
+            <Button
+              type="button"
+              data-testid="btn-play-again"
+              style={ { background: 'black', color: 'white' } }
+              onClick={ () => {
+                reset();
+                history.push('/');
+              } }
+            >
+              Play Again
+            </Button>
+            <Button
+              type="button"
+              data-testid="btn-ranking"
+              style={ { background: 'black', color: 'white' } }
+              onClick={ () => {
+                history.push('/ranking');
+              } }
+            >
+              Ranking
+            </Button>
+          </Box>
+        </Box>
+        <Footer />
+      </Box>
     );
   }
 }
