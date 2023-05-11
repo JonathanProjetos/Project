078f96c5f0258013568428e11eac9d7d5b616bbb
Jonathan Santos <jonathan01347@gmail.com>
2023-05-11 00:42:52 -0300

    refactor(Ranking)convert for react func

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import { resetState } from '../redux/actions';
+import Footer from '../componets/Footer';
 import Players from '../componets/Players';
+import useQueryMedia from '../hook/useQueryMedia';
 
-class Ranking extends Component {
-  render() {
-    const { reset, history } = this.props;
-    return (
-      <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <Players />
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ () => {
-            reset();
-            history.push('/');
-          } }
-        >
-          Inicio
-        </button>
-      </div>
-    );
-  }
+function Ranking({ reset, history }) {
+  const isSmallScreen = useQueryMedia();
+  return (
+    <Box
+      sx={ {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      } }
+    >
+      <Typography
+        variant="h2"
+        data-testid="ranking-title"
+        style={ {
+          marginBottom: '30px',
+          marginTop: '30px',
+          fontWeight: 'bold',
+        } }
+      >
+        Ranking
+      </Typography>
+      <Players />
+      <Button
+        type="button"
+        style={ { background: 'black', color: 'white', marginBottom: '100px' } }
+        data-testid="btn-go-home"
+        onClick={ () => {
+          reset();
+          history.push('/');
+        } }
+      >
+        Inicio
+      </Button>
+      { isSmallScreen && (<Footer />)}
+    </Box>
+  );
 }
 
 Ranking.propTypes = {
