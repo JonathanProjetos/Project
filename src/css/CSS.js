f2f255610f5fc0aecd83fb6ce082960ef7e404d4
Jonathan Santos <jonathan01347@gmail.com>
2023-05-26 19:24:42 -0300

    feat(Css)create function for style

---
+++ src/css/CSS.js
@@ -0,0 +1,32 @@
+const style = () => ({
+  style1: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    textAlign: 'center',
+    width: '100vw',
+    height: '100vh',
+    alignItems: 'center',
+  },
+  style2: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    width: '600px',
+    height: '600px',
+    alignItems: 'center',
+    marginTop: '20px',
+  },
+  style3: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'between',
+  },
+  style4: {
+    color: 'black',
+    background: 'white',
+  },
+});
+
+export default style;
