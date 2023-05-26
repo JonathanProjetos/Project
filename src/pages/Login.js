9e1661b083bd2a7c73c612b40ac6488705bf9641
Jonathan Santos <jonathan01347@gmail.com>
2023-05-26 19:26:11 -0300

    refactor(Login)converted from react class to react function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,162 +1,125 @@
 /* eslint-disable react/sort-comp */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Box from '@material-ui/core/Box';
+import { useHistory } from 'react-router-dom';
 import Image from '../componets/Image';
 import logo from '../trivia.png';
 import fetchToken from '../helper/fetchToken';
 import { actionSetTokem, actionSetUser } from '../redux/actions';
 import Footer from '../componets/Footer';
+import style from '../css/CSS';
 
-class Login extends Component {
-  constructor() {
-    super();
+function Login({ setToken, setUser }) {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
 
-    this.state = {
-      email: '',
-      name: '',
-      isDisabled: true,
-    };
-  }
+  useEffect(() => {
+    let isMounted = true;
 
-  handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value }, () => {
-      const { email, name } = this.state;
-      const MIN_LENGTH = 1;
+    const validate = () => {
+      const MIN_LENGTH = 6;
       const emailCheck = email
         .split('').includes('@') && email.split('.').includes('com');
       const nameCheck = name.length >= MIN_LENGTH;
-      if (emailCheck && nameCheck) {
-        this.setState({ isDisabled: false });
+      if (emailCheck && nameCheck && isMounted) {
+        setIsDisabled(false);
       } else {
-        this.setState({ isDisabled: true });
+        setIsDisabled(true);
       }
-    });
-  }
+    };
 
-  handleSubmit = async (e) => {
+    validate();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [email, name]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const { setToken, setUser, history } = this.props;
     const token = await fetchToken();
-    const { email, name } = this.state;
     setToken(token);
     setUser({ email, name });
     localStorage.setItem('name', name);
     history.push('/game');
-  }
+  };
 
-  handleClick = () => {
-    const { history } = this.props;
-    history.push('/settings');
-  }
-
-  style = () => ({
-    style1: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      textAlign: 'center',
-      width: '100vw',
-      height: '100vh',
-      alignItems: 'center',
-    },
-    style2: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      textAlign: 'center',
-      width: '600px',
-      height: '600px',
-      alignItems: 'center',
-      marginTop: '20px',
-    },
-    style3: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'between',
-    },
-    style4: {
-      color: 'black',
-      background: 'white',
-    },
-  })
-
-  render() {
-    const { email, name, isDisabled } = this.state;
-    return (
+  return (
+    <Box
+      component="div"
+      sx={ {
+        ...style().style1,
+      } }
+    >
       <Box
-        component="div"
+        component="form"
+        onSubmit={ handleSubmit }
         sx={ {
-          ...this.style().style1,
+          ...style().style2,
         } }
       >
-        <Box
-          component="form"
-          onSubmit={ this.handleSubmit }
-          sx={ {
-            ...this.style().style2,
-          } }
-        >
-          <Box>
-            <Image src={ logo } alt="logo" />
-          </Box>
-          <Box sx={ { ...this.style.style3, marginTop: '50px' } }>
-            <InputLabel htmlFor="name">
-              <Input
-                style={ {
-                  marginBottom: '20px',
-                  width: '300px',
-                  padding: '1px',
-                  animation: 'unset',
-                } }
-                id="name"
-                name="name"
-                type="text"
-                placeholder="Digite seu nome"
-                data-testid="input-player-name"
-                onChange={ this.handleChange }
-                value={ name }
-              />
-            </InputLabel>
-            <InputLabel htmlFor="email">
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Digite seu email"
-                data-testid="input-gravatar-email"
-                onChange={ this.handleChange }
-                value={ email }
-                style={ {
-                  background: 'transparent',
-                  marginBottom: '20px',
-                  width: '300px',
-                  padding: '1px',
-                  textAlign: 'center',
-                } }
-              />
-            </InputLabel>
-          </Box>
-          <Box sx={ { ...this.style().style3, marginTop: '30px' } }>
-            <Button
-              style={ { background: 'black', color: 'white' } }
-              variant="contained"
-              onClick={ this.handleSubmit }
-              data-testid="btn-play"
-              disabled={ isDisabled }
-            >
-              {isDisabled ? 'Close' : 'Vamos Jogar?'}
-            </Button>
-          </Box>
+        <Box>
+          <Image src={ logo } alt="logo" />
         </Box>
-        <Footer />
+        <Box sx={ { ...style().style3, marginTop: '50px' } }>
+          <InputLabel htmlFor="name">
+            <Input
+              style={ {
+                marginBottom: '20px',
+                width: '300px',
+                padding: '1px',
+                animation: 'unset',
+              } }
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Digite seu nome"
+              data-testid="input-player-name"
+              onChange={ ({ target }) => setName(target.value) }
+              value={ name }
+            />
+          </InputLabel>
+          <InputLabel htmlFor="email">
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Digite seu email"
+              data-testid="input-gravatar-email"
+              onChange={ ({ target }) => setEmail(target.value) }
+              value={ email }
+              style={ {
+                background: 'transparent',
+                marginBottom: '20px',
+                width: '300px',
+                padding: '1px',
+                textAlign: 'center',
+              } }
+            />
+          </InputLabel>
+        </Box>
+        <Box sx={ { ...style().style3, marginTop: '30px' } }>
+          <Button
+            style={ { background: 'black', color: 'white' } }
+            variant="contained"
+            onClick={ handleSubmit }
+            data-testid="btn-play"
+            disabled={ isDisabled }
+          >
+            {isDisabled ? 'Close' : 'Vamos Jogar?'}
+          </Button>
+        </Box>
       </Box>
-    );
-  }
+      <Footer />
+    </Box>
+  );
 }
 
 Login.propTypes = {
