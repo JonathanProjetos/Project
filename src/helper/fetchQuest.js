fdb0baca6dab192ef1a72df44e48efb31261bd09
Jonathan Santos <jonathan01347@gmail.com>
2023-05-28 16:55:48 -0300

    fix(fetchQuest)fixing questions encoding to utf-8

--- src/helper/fetchQuest.js
+++ src/helper/fetchQuest.js
@@ -1,12 +1,31 @@
+/* eslint-disable camelcase */
+/* eslint-disable prefer-const */
+import he from 'he';
+
 const fetchQuest = async (token) => {
   const URL = `https://opentdb.com/api.php?amount=5&token=${token}`;
 
-  const question = await fetch(URL)
-    .then((response) => response.json())
-    .then((response) => response.results);
-    // .catch((erro) => erro);
+  const messageQuestion = await fetch(URL);
+  const response = await messageQuestion.json();
+  const data = response.results;
+  // console.log(data);
 
-  return question;
+  const filterdata = data.map((item) => {
+    let {
+      question,
+      category,
+      correct_answer,
+      difficulty,
+      incorrect_answers,
+      type } = item;
+
+    const utf8Text = he.decode(question);
+    const text = utf8Text.split(' ,')[0];
+    question = text;
+    return { question, category, correct_answer, difficulty, incorrect_answers, type };
+  });
+
+  return filterdata;
 };
 
 export default fetchQuest;
